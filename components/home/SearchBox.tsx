85df20a0a2c2749522e68e32765e574e1b525e1f
Umut Berkay Soydan <ubsoydan@protonmail.com>
2023-04-29 16:10:11 +0300

    update: card ui for SearchBox comp

--- components/home/SearchBox.tsx
+++ components/home/SearchBox.tsx
@@ -1,18 +1,37 @@
-import Card from "components/ui/feature-card";
+import {
+    Card,
+    CardContent,
+    CardTitle,
+    CardHeader,
+    CardDescription,
+} from "components/ui/card";
 import { Input } from "components/ui/input";
 import { Button } from "components/ui/button";
 
 export default function SearchBox() {
     return (
-        <div className="w-[50rem]">
-            <Card
-                title="Sorgula"
-                description="Bilmedigin bir harcamayi buradan sorgulayabilirsin"
-                reverseOrder
-            >
-                <Input />
-                <Button className="ml-6">Sorgula</Button>
-            </Card>
-        </div>
+        <Card className="w-[48rem]">
+            <CardHeader>
+                <CardTitle>Sorgula</CardTitle>
+                <CardDescription>
+                    Banka dekontunuzda veya harcama detayinda yer alan
+                    harcamanin ismini girerek bilinmeyen harcamanizi
+                    ogrenebilirsiniz.
+                </CardDescription>
+            </CardHeader>
+            <CardContent>
+                <div className="flex">
+                    <form action="/api/charges/[id]" method="get">
+                        <Input
+                            type="text"
+                            placeholder="Bilinmeyen harcamanizi girin..."
+                        />
+                        <Button type="submit" className="ml-6 px-10">
+                            Sorgula
+                        </Button>
+                    </form>
+                </div>
+            </CardContent>
+        </Card>
     );
 }
