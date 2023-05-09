1d671a279567975b3e2ed584dd63e2c56c8f6029
Umut Berkay Soydan <ubsoydan@protonmail.com>
2023-05-10 01:32:18 +0300

    improve: add some improvements

--- components/home/FraudCheckForm.tsx
+++ components/home/FraudCheckForm.tsx
@@ -4,16 +4,18 @@ import { Button } from "components/ui/button";
 import { useState } from "react";
 import { useRouter } from "next/navigation";
 
-export default function SearchForm() {
-    const [chargeData, setChargeData] = useState("");
+export default function FraudCheckForm() {
+    const [chargeToCheck, setChargeToCheck] = useState("");
     const router = useRouter();
 
     async function inquireCharge(event: React.FormEvent) {
         event.preventDefault();
 
+        // Format query parameter with dashes
+        const queryParam = chargeToCheck.replace(/\s+/g, "-");
         try {
             // Format user input to be used as query param
-            const id = encodeURI(chargeData);
+            const id = encodeURI(chargeToCheck);
             // Inquiry
             const res = await fetch(`/api/charges/${id}`, {
                 method: "GET",
@@ -27,8 +29,8 @@ export default function SearchForm() {
                 console.log("BOYLE BIR KAYIT VAR", existingCharge);
 
                 // Formats url for readibility, swaps %20s with dashes
-                const redirectTo = id.replace(/%20/g, "-");
-                router.replace(`/${redirectTo}`);
+                // const redirectTo = id.replace(/%20/g, "-");
+                router.push(`/check?q=${queryParam}`);
             }
 
             if (!existingCharge) {
@@ -39,28 +41,31 @@ export default function SearchForm() {
                         "Content-Type": "application/json",
                     },
                     body: JSON.stringify({
-                        charge: chargeData,
+                        charge: chargeToCheck,
                     }),
                 });
 
                 const newCharge = await res.json();
                 console.log("new charge created", newCharge);
-                router.replace(`/${newCharge.name}`);
+
+                router.push(`/check?q=${queryParam}`);
             }
-        } catch (err) {}
+        } catch (err) {
+            console.log(err);
+            return new Error();
+        }
     }
+
     return (
-        <div className="flex">
-            <form onSubmit={inquireCharge}>
+        <div className="flex flex-col items-center">
+            <form onSubmit={inquireCharge} className="w-full">
                 <Input
                     type="text"
                     placeholder="Bilinmeyen harcamanizi girin..."
-                    onChange={(e) => setChargeData(e.target.value)}
-                    value={chargeData}
+                    onChange={(e) => setChargeToCheck(e.target.value)}
+                    value={chargeToCheck}
                 />
-                <Button type="submit" className="ml-6 px-10">
-                    Sorgula
-                </Button>
+                <Button type="submit">Sorgula</Button>
             </form>
         </div>
     );
