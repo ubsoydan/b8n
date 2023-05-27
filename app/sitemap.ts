b8f60183dfe853a4da500abb36519c272b9f51b3
Umut Berkay Soydan <ubsoydan@protonmail.com>
2023-05-27 14:15:08 +0300

    add: lastModified info for sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -2,23 +2,21 @@ import { db } from "@/lib/db";
 
 // Fetch the names of all charges on server-side via prisma
 async function fetchCharges() {
-    const allCharges = await db.charge.findMany({
-        select: { name: true },
+    const res = await db.charge.findMany({
+        select: { name: true, updatedAt: true },
     });
-    return allCharges;
-}
-
-// Make URLs out of charge names
-async function generateUrls() {
-    const data = await fetchCharges();
-    const URLs = data.map((charge) => encodeURIComponent(charge.name));
-    return URLs;
+    const charges = res.map((charge) => ({
+        url: encodeURIComponent(charge.name),
+        updatedAt: charge.updatedAt,
+    }));
+    return charges;
 }
 
 export default async function sitemap() {
-    const allCharges = await generateUrls();
+    const allCharges = await fetchCharges();
     const charges = allCharges.map((charge: any) => ({
-        url: `https://bilinmeyenharcama.com/${charge}`,
+        url: `https://bilinmeyenharcama.com/${charge.url}`,
+        lastModified: new Date(charge.updatedAt).toISOString().split("T")[0],
     }));
 
     const routes = [
@@ -29,6 +27,7 @@ export default async function sitemap() {
         "/check",
     ].map((route) => ({
         url: `https://bilinmeyenharcama.com${route}`,
+        lastModified: new Date().toISOString().split("T")[0],
     }));
 
     return [...routes, ...charges];
